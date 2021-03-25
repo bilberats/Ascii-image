9262e6aa3a45f694f1ca19b60e4ac2744c2c8b61
bilberats <81383654+bilberats@users.noreply.github.com>
2021-03-25 20:22:13 +0100

    Add files via upload

---
+++ script-detail.py
@@ -0,0 +1,52 @@
+import PIL.Image
+
+#ascii chars
+ASCII_CHARS = ['$','@','B','%','8','&','W','M','#','*','o','a','h','k','b','d','p','q','w','m','Z','O','0','Q','L','C','J','U','Y','X','z','c','v','u','n','x','r','j','f','t','/','|','(',')','1','{','}','[',']','?','-','_','+','~','i','!','l',';',':',',','\"','^','`','.']
+
+#resize
+def resize_image(image,new_width):
+    width, height = image.size
+    ratio = height / width / 1.80
+    new_height = int(new_width*ratio)
+    resized_image = image.resize((new_width, new_height))
+    return resized_image
+
+#convert each pixel to grayscale
+def grayify(image):
+    grayscale_image = image.convert("L")
+    return grayscale_image
+
+#convert pixels to ascii
+def pixel_to_ascii(image):
+    pixels = image.getdata()
+    characters = "".join([ASCII_CHARS[int(pixel//(255/(len(ASCII_CHARS)-1)))] for pixel in pixels])
+    return characters
+
+
+def main():
+    path = input("Image path:\n")
+    new_width = int(input("Ascii image width :\n"))
+    try:
+        image = PIL.Image.open(path)
+    except:
+        print(path, "Is not a valid path !")
+
+
+    #convert image to ascii
+    new_image_data = pixel_to_ascii(grayify(resize_image(image,new_width)))
+
+    #format
+    pixel_count = len(new_image_data)
+    ascii_image = "\n".join(new_image_data[i:(i+new_width)] for i in range(0, pixel_count, new_width))
+
+
+    print(ascii_image)
+
+
+    with open("ascii_image.txt", "w") as f:
+        f.write(ascii_image)
+
+
+main()
+
+a = input("Appuyez sur une touche pour fermer...")
